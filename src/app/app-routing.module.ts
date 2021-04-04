5d66caaaf307dfe0a599ae4afe633eb493bd5f16
AlishStrong <alishet1990@gmail.com>
2021-04-04 16:20:39 +0300

    Checked that primitive routing is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeModule } from './modules/home/home.module';
+import { ProductsModule } from './modules/products/products.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'products', loadChildren: () => ProductsModule },
+  { path: 'home', loadChildren: () => HomeModule },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
