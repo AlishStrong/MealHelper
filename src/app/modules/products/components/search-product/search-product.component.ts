253561e8efadfbc29f9fa2938ead3c0b453c2174
AlishStrong <alishet1990@gmail.com>
2021-04-16 02:02:47 +0300

    Added reactive search support

--- src/app/modules/products/components/search-product/search-product.component.ts
+++ src/app/modules/products/components/search-product/search-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search-product',
@@ -10,12 +12,22 @@ export class SearchProductComponent implements OnInit {
   dataset = ['MDB', 'Angular', 'Bootstrap', 'Framework', 'SPA', 'React', 'Vue'];
   searchItem: string;
   
+  reactiveSearch = new FormControl('');
+  reactiveDataset = new Subject<string[]>();
   constructor() { }
 
   ngOnInit() {
+    this.reactiveSearch.valueChanges.subscribe(this.imitateServerRequest());
   }
 
   selectProduct($event): void {
     console.log(`You selected ${$event}`);
+  }
+
+  private imitateServerRequest(): (value: any) => void {
+    return (toFind: string) => {
+      console.log(`Requesting for ${toFind}`);
+      this.reactiveDataset.next(toFind ? this.dataset.filter((item: string) => item.toLocaleLowerCase().includes(toFind.toLocaleLowerCase())) : []);
+    };
   }
 }
