55d3ad42962028cc838516cf69476df77203d3f2
AlishStrong <alishet1990@gmail.com>
2021-04-22 01:43:52 +0300

    Products can be added and removed to a Recipe in the form. There are several TODOs. AddRecipe is not done yet!

--- src/app/modules/recipes/components/add-recipe/add-recipe.component.ts
+++ src/app/modules/recipes/components/add-recipe/add-recipe.component.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, iif, Observable, of } from 'rxjs';
+import { iif, Observable, of } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
-import { Food } from 'src/app/shared/models/food.model';
 import { Product } from 'src/app/shared/models/product.model';
 import { Ingredient, Ingredients, Recipe } from 'src/app/shared/models/recipe.model';
 
@@ -14,44 +13,26 @@ import { Ingredient, Ingredients, Recipe } from 'src/app/shared/models/recipe.mo
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddRecipeComponent implements OnInit {
-  private foodMock: Product[] = [
-    {
-      name: 'Banana',
-      brand: 'Fairtrade',
-      caloriePer100gr: 23,
-      proteinPer100gr: 0.56,
-      carbohydratesPer100gr: 3.45,
-      fatPer100gr: 0.3
-    },
-    {
-      name: 'Orange',
-      brand: '',
-      caloriePer100gr: 45,
-      proteinPer100gr: 0.2,
-      carbohydratesPer100gr: 9.3,
-      fatPer100gr: 0.01
-    }
-  ];
+  public recipeForm: FormGroup;
+  public ingredientList: Ingredients = new Array<Ingredient>();
 
-  ingredientList: Ingredients = new Array<Ingredient>();
+  public products$: Observable<Product[]>;
+  public showProductList = true;
 
-  products$: Observable<Product[]>;
-  showProductList = true;
   private readonly _productsJSON = 'assets/products-mock.json';
 
-  recipeForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    ingredients: new FormControl(this.ingredientList, Validators.required),
-    ingredientName: new FormControl('', [Validators.required]),
-    ingredientBrand: new FormControl(''),
-    ingredientAmount: new FormControl(null, [Validators.required, Validators.min(1)])
-  });
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.recipeForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      ingredients: new FormControl(this.ingredientList, Validators.required),
+      ingredientName: new FormControl('', [Validators.required]),
+      ingredientBrand: new FormControl(''),
+      ingredientAmount: new FormControl({ value: null, disabled: this.showProductList }, [Validators.required, Validators.min(1)])
+    });
+  }
 
   ngOnInit() {
-    this.products$ = this.recipeForm.get('ingredientName').valueChanges.pipe(
-      tap(console.log),
+    this.products$ = this.recipeForm.controls.ingredientName.valueChanges.pipe(
       switchMap((searchWord: string) => iif(
         () => !!searchWord.trim(),
         this.findProducts(searchWord),
@@ -61,60 +42,48 @@ export class AddRecipeComponent implements OnInit {
     );
   }
 
-  private findProducts(searchWord: string) {
-    return this.http.get<Product[]>(this._productsJSON).pipe(
-      map(products => {
-        const toReturn = products.filter((product: Product) => product.name.toLocaleLowerCase().includes(searchWord.toLocaleLowerCase()));
-        const currentName = this.recipeForm.get('ingredientName').value;
-        console.log(`CurrentName ${currentName}`);
-        if (toReturn.filter((p: Product) => p.name === currentName).length === 0) {
-          console.log(`Product list does not have CurrentName - ${currentName}`);
-          this.showProductList = true;
-        } else {
-          console.log(`Product list includes CurrentName - ${currentName}`);
-          this.showProductList = false;
-        }
-        return toReturn;
-      }),
-      // tap((products: Product[]) => {
-      //   console.log(`Searching`);
-      //   if (products && // server returned a list of products
-      //     products.length > 0 && // the returned list of products is not empty
-      //     products.filter((pr: Product) => {
-      //       return pr.name === this.recipeForm.get('ingredientName').value;
-      //     }).length === 0 // the list does not contain already selected ingredient
-      //   ) {
-      //     console.log(`showProductList$ TRUE`);
-      //     this.showProductList$.next(true);
-      //   } else {
-      //     console.log(`showProductList$ FALSE`);
-      //     this.showProductList$.next(false);
-      //   }
-      // })
-    );
-  }
-
   selectProduct(product: Product): void {
-    console.log(`You selected ${product.name}`);
-    this.recipeForm.get('ingredientName').setValue(product.name);
-    this.recipeForm.get('ingredientBrand').setValue(product.brand);
+    this.recipeForm.controls.ingredientName.setValue(product.name);
+    this.recipeForm.controls.ingredientBrand.setValue(product.brand);
+    this.recipeForm.controls.ingredientAmount.enable({ onlySelf: true });
   }
 
+  // TODO check that ingredient is already in the list or not
+  // TODO update caloric and macro info of the Recipe
+  addIngredient(): void {
+    this.ingredientList.push({
+      name: this.recipeForm.controls.ingredientName.value,
+      brand: this.recipeForm.controls.ingredientBrand.value,
+      amount: this.recipeForm.controls.ingredientAmount.value
+    });
+    this.recipeForm.controls.ingredientName.reset('');
+    this.recipeForm.controls.ingredientBrand.reset();
+    this.recipeForm.controls.ingredientAmount.reset();
+  }
+
+  // TODO use Product as a parameter
+  // TODO update caloric and macro info of the Recipe
+  removeIngredient(ingredientName: string): void {
+    this.ingredientList = this.ingredientList.filter((ingredient: Ingredient) => ingredient.name !== ingredientName);
+  }
+
+  // TODO clear the form when a Recipe is added
   addNewRecipe(): void {
     const newRecipe = Recipe.fromFormGroupValue(this.recipeForm.value);
     console.log(newRecipe);
   }
 
-  removeIngredient(ingredientName: string): void {
-    // this.ingredientList.delete(ingredientName);
-  }
-
-  addIngredient(): void {
-    // this.ingredientList.set(
-    //   this.recipeForm.controls.ingredientName.value,
-    //   this.recipeForm.controls.ingredientAmount.value,
-    // );
-    // this.recipeForm.controls.ingredientName.reset();
-    // this.recipeForm.controls.ingredientAmount.reset();
+  private findProducts(searchWord: string) {
+    return this.http.get<Product[]>(this._productsJSON).pipe(
+      map(products => products.filter((product: Product) => product.name.toLocaleLowerCase().includes(searchWord.toLocaleLowerCase()))),
+      tap((products: Product[]) => {
+        const currentName = this.recipeForm.controls.ingredientName.value;
+        if (products.filter((p: Product) => p.name === currentName).length === 0) {
+          this.showProductList = true;
+        } else {
+          this.showProductList = false;
+        }
+      })
+    );
   }
 }
